0de0d390cc6ef33717d7d9cbb234bddb1f5a57bd
Ghazal Shaikh <159722197+Ghazal010@users.noreply.github.com>
2025-02-07 03:42:46 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-// import { GeistSans, GeistMono } from "next/font/google";
+ import { GeistSans, GeistMono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer";
@@ -10,15 +10,15 @@ import { Inter } from "next/font/google";
 
  const inter = Inter({ subsets: ["latin"] });
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "BuyNBuy Vibes",
